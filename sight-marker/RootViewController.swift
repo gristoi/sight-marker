b809cc7babd66255fd8f301c43854af5bfce71fa
Ian Gristock <iangristock@gmail.com>
2016-11-21 17:28:37 +0000

    comitting latest

--- sight-marker/RootViewController.swift
+++ sight-marker/RootViewController.swift
@@ -9,21 +9,50 @@
 import UIKit
 import RealmSwift
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let realm = try! Realm()
     var bow: Bow?
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bowNameLabel: UILabel!
+    @IBOutlet weak var bowTypeLabel: UILabel!
+    @IBOutlet weak var bowDrawWeightLabel: UILabel!
     open override func viewDidLoad() {
         super.viewDidLoad()
-        let predicate = NSPredicate(format: "isDefault = %@", 1)
+        let predicate = NSPredicate(format: "isDefault = %ld", 1)
         bow = realm.objects(Bow.self).filter(predicate).first
-        print(bow)
-        //view.backgroundColor = Color.grey.lighten5
+        bowNameLabel.text = bow?.riser
+        bowTypeLabel.text = bow?.bow
+        bowDrawWeightLabel.text = "Draw Weight : \(bow!.drawWeight) lbs"
+        let sighting  = Sighting()
+        sighting.distance = 10
+        sighting.marking = 1.9
+        sighting.hole = 0
+        bow?.sightings.append(sighting)
+        tableView.reloadData()
 
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bow?.sightings.count ?? 0
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Getting the right element
+        let element = bow?.sightings[indexPath.row]
+        
+        // Instantiate a cell
+        let cellIdentifier = "ElementCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+            ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        // Adding the right informations
+        cell.textLabel?.text = String(describing: element?.distance)
+        cell.detailTextLabel?.text = String(describing: element?.marking)
+        
+        // Returning the cell
+        return cell
+    }
 
 }
 
